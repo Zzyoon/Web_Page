073eec8c1f35f381a83e402226e01dff84543b46
zzyoon <develop_z@naver.com>
2022-07-01 01:54:33 +0900

    16

---
+++ syntax/template.js
@@ -0,0 +1,15 @@
+var tem = 'hot or iced';
+var letter1 = 'coffee ' + tem +'\ncold brew iced only \ngranita \ngelato \ntea ' + tem;
+
+//console.log(letter1);
+
+// `` : 템플릿 리터럴 시작과 끝을 나타내는 기호
+
+var letter2 = `coffee ${tem} 
+
+cold brew iced only 
+granita 
+gelato 
+tea ${tem}`;
+
+console.log(letter2);
---
+++ syntax/variable.js
@@ -0,0 +1,4 @@
+var tem = 'hot or iced';
+var letter = 'coffee ' + tem +' cold brew iced only granita gelato tea ' + tem;
+
+console.log(letter);
