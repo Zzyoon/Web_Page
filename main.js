1d93f4fd280c912bf1b474161018985dd56b9f66
zyoon <develop_z@naver.com>
2022-10-20 23:48:25 +0900

    34

--- main.js
+++ main.js
@@ -2,19 +2,46 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); //url변수를 통해 url이란 모듈을 사용할거야!
 
+function templateHTML(title, list, body){
+    return `
+        <!doctype html>
+        <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+        </body>
+        </html>
+        `;
+}
+
+function templateList(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i++;
+    }
+    list = list + '</ul>';
+}
+
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query; //_url정보 중에서 query값만 보여주기
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
 
     if(pathname === '/'){ //pathname으로는 home과 각각 페이지들을 구분 불가 (모두 다 /니까)
 
-        if(title === undefined){
+        if(queryData.id === undefined){
             fs.readdir('./data', function(error, filelist){
-                title = "Welcome";
-                description = "Hello! Node.js";
+                var title = "Welcome";
+                var description = "Hello! Node.js";
 
                 /*
                 var list = `
@@ -23,7 +50,7 @@ var app = http.createServer(function(request,response){
                     <li><a href="/?id=CSS">CSS</a></li>
                     <li><a href="/?id=JavaScript">JavaScript</a></li>
                     </ol>`;
-                */
+                
                 var list = '<ul>';
                 var i = 0;
                 while(i < filelist.length){
@@ -31,22 +58,10 @@ var app = http.createServer(function(request,response){
                     i++;
                 }
                 list = list + '</ul>';
+                */
 
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-                `;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
                 response.writeHead(200); //성공
                 //사용자에게 보여줄 내용 response.end
                 response.end(template);
@@ -55,9 +70,8 @@ var app = http.createServer(function(request,response){
         }
         else{
             fs.readdir('./data', function(error, filelist){
-                title = "Welcome";
-                description = "Hello! Node.js";
-
+                
+                /*
                 var list = '<ul>';
                 var i = 0;
                 while(i < filelist.length){
@@ -65,24 +79,13 @@ var app = http.createServer(function(request,response){
                     i++;
                 }
                 list = list + '</ul>';
-
+                */
+            
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                
             response.writeHead(200); //성공
             //사용자에게 보여줄 내용 response.end
             response.end(template);
---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6)); //Math.round 반올림 함수
+
+function sum(a, b){ //parameter 매개변수
+    return a+b; //return만나면 바로 함수 종료
+}
+
+console.log(sum(2,4)); //argument 인자
+
+
